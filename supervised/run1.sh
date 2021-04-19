bc18cfbb3b7b673f09a174f27b167b8a9bf66681
LeeMyungHak <mh9716@naver.com>
2021-04-19 09:59:20 +0900

    Update run1.sh

--- supervised/run1.sh
+++ supervised/run1.sh
@@ -1,5 +1,5 @@
 # +
-CUDA_VISIBLE_DEVICES=1 python main.py --dataset cifar100 --data_dir data/cifar100/ --root_dir experiments/ --labels_per_class 500 --arch wrn28_10  --learning_rate 0.1 --momentum 0.9 --decay 0.0001 --epochs 400 --schedule 200 300 --gammas 0.1 0.1 --train vanilla --random_rate 1.0 --class_num 100
+# CUDA_VISIBLE_DEVICES=1 python main.py --dataset cifar100 --data_dir data/cifar100/ --root_dir experiments/ --labels_per_class 500 --arch wrn28_10  --learning_rate 0.1 --momentum 0.9 --decay 0.0001 --epochs 400 --schedule 200 300 --gammas 0.1 0.1 --train vanilla --random_rate 1.0 --class_num 100
 
 CUDA_VISIBLE_DEVICES=1 python main.py --dataset cifar100 --data_dir data/cifar100/ --root_dir experiments/ --labels_per_class 500 --arch wrn28_10  --learning_rate 0.1 --momentum 0.9 --decay 0.0001 --epochs 400 --schedule 200 300 --gammas 0.1 0.1 --train vanilla --random_rate 0.95 --class_num 100
 
